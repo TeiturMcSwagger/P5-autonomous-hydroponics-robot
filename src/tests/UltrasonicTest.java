90dc8de160cd67cf07506bef08d02ca5ae797cf4
Lau Kondrup <lau.kondrup@gmail.com>
2017-09-21 11:13:08 +0200

    Ultrasonic sensor test

--- src/tests/UltrasonicTest.java
+++ src/tests/UltrasonicTest.java
@@ -3,15 +3,43 @@ package tests;
 import lejos.nxt.*;
 import lejos.nxt.comm.RConsole;
 
+/**
+ * Tests the consistency and latency of ultrasonic sensors
+ */
 public class UltrasonicTest {
+    private static UltrasonicSensor ultrasonicSensor;
+
     public static void main(String[] args) {
-        UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S1);
+        ultrasonicSensor = new UltrasonicSensor(SensorPort.S1);
         RConsole.println("Connecting to USB");
         RConsole.openUSB(20000);
         RConsole.println("Connected to USB");
 
-        while(Button.readButtons() != Button.ID_ESCAPE) {
-            RConsole.println("" + ultrasonicSensor.getDistance());
+        int i = 0;
+        while(Button.readButtons() != Button.ID_ESCAPE || i == 1000) {
+            getDistance_Test();
+//        latency_Test();
+            i++;
         }
+    }
+
+    /**
+     * Tests how consistent the distance measurements are
+     */
+    private static void getDistance_Test() {
+        RConsole.println("Testing distance consistency");
+        RConsole.println(Double.toString(ultrasonicSensor.getDistance()));
+    }
+
+    /**
+     * Tests how often the Ultrasonic Sensor can be pinged
+     */
+    private static void latency_Test() {
+        RConsole.println("Testing distance latency");
+        double startTime = System.nanoTime();
+        ultrasonicSensor.getDistance();
+        double endTime = System.nanoTime();
+        double latency = endTime - startTime;
+        RConsole.println(Double.toString(latency));
     }
 }
