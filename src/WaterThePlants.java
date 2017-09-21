aad0d83209c1eb73ba035e6c4f144c29e07d13cf
Lau Kondrup <lau.kondrup@gmail.com>
2017-09-21 13:43:24 +0200

    Water plant stuff :snowman:

--- src/WaterThePlants.java
+++ src/WaterThePlants.java
@@ -4,22 +4,41 @@ import lejos.nxt.comm.RConsole;
 import static tests.NXTTest.connectBluetooth;
 
 public class WaterThePlants {
-    public static void main(String[] args) {
-        UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S1);
+    private static NXTRegulatedMotor motor;
+    private static NXTRegulatedMotor motorArm;
 
+    public static void main(String[] args) {
+        ColorSensor colorSensor = new ColorSensor(SensorPort.S1);
+        motor = Motor.A;
+        motorArm = Motor.B;
         connectBluetooth();
-        RConsole.println("Initiating..");
+        RConsole.println("Expecting color sensor to be in port 1");
+        RConsole.println("Expecting motor to be in port A");
 
         while(Button.readButtons() != Button.ID_ESCAPE) {
-            Motor.A.forward();
-            int distance = ultrasonicSensor.getDistance();
-            System.out.println(distance);
+            // drive indefinitely
+            motor.forward();
+
+            // scan color
+            ColorSensor.Color color = colorSensor.getColor();
+            if(color.toString() == "stuff") {
+                motor.stop();
+                waterThePlant();
+            }
 
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
         }
+    }
+
+    private static void waterThePlant() {
+        RConsole.println("Watering the plant..");
+        motorArm.rotate(90);
+        // wait
+        // rotate back
     }
 }
