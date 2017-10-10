3082b41a375c49bc4872ac282f2b9f67866845e6
Lau Kondrup <lau.kondrup@gmail.com>
2017-10-10 13:09:24 +0200

    Voltage test fixed

--- src/tests/MotorVoltageTest.java
+++ src/tests/MotorVoltageTest.java
@@ -1,18 +1,15 @@
 package tests;
 
-
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 
 import static lejos.nxt.Battery.getVoltageMilliVolt;
-import static tests.NXTTest.connectBluetooth;
 
 public class MotorVoltageTest {
     private static NXTRegulatedMotor motor;
 
     public static void main(String[] args) throws InterruptedException
     {
-        connectBluetooth();
         motor = Motor.A;
         double v1 = getVoltageMilliVolt();
         System.out.println("Idle volt: " + v1);
@@ -23,5 +20,10 @@ public class MotorVoltageTest {
         double v2 = getVoltageMilliVolt();
         motor.stop();
         System.out.println("Running volt: " + v2);
+        Thread.sleep(5000);
+        while(true) {
+            v2 = getVoltageMilliVolt();
+            System.out.println("Running volt: " + v2);
+        }
     }
 }
