21fb3be322ae51247ff6fac4b7c86afec7b20005
Lau Kondrup <lau.kondrup@gmail.com>
2017-10-10 13:11:24 +0200

    Fixed fo realz

--- src/tests/MotorVoltageTest.java
+++ src/tests/MotorVoltageTest.java
@@ -1,28 +1,26 @@
 package tests;
 
+import lejos.nxt.Battery;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
-
-import static lejos.nxt.Battery.getVoltageMilliVolt;
-
 public class MotorVoltageTest {
     private static NXTRegulatedMotor motor;
 
     public static void main(String[] args) throws InterruptedException
     {
         motor = Motor.A;
-        double v1 = getVoltageMilliVolt();
+        double v1 = Battery.getVoltageMilliVolt();
         System.out.println("Idle volt: " + v1);
         motor.setSpeed(900);
 //        motor.rotate(360);
         motor.forward();
         Thread.sleep(500);
-        double v2 = getVoltageMilliVolt();
+        double v2 = Battery.getVoltageMilliVolt();
         motor.stop();
         System.out.println("Running volt: " + v2);
         Thread.sleep(5000);
         while(true) {
-            v2 = getVoltageMilliVolt();
+            v2 = Battery.getVoltageMilliVolt();
             System.out.println("Running volt: " + v2);
         }
     }
