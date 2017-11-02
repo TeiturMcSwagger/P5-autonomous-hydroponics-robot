84fd44e1d6039cbd6040197577c8b048fab518c4
Peter Madsen <pmadse13@student.aau.dk>
2017-11-02 15:04:31 +0100

    hehe implicit decl

--- testCode/poc/main.c
+++ testCode/poc/main.c
@@ -1,9 +1,9 @@
 /* rms.c */
-#include <stdlib.h>
+#include "ecrobot_interface.h"
 #include "kernel.h"
 #include "kernel_id.h"
-#include "ecrobot_interface.h"
 #include "nxtMotorController.h"
+#include <stdlib.h>
 
 /* OSEK declarations */
 
@@ -17,30 +17,24 @@
 }*/
 
 /* LEJOS OSEK hook to be invoked from an ISR in category 2 */
-void user_1ms_isr_type2(void)
-{
-}
+void user_1ms_isr_type2(void) {}
 
-void printString(char* str, int row)
-{
+void printString(char *str, int row) {
   display_goto_xy(0, row);
   display_string(str);
   display_update();
 }
 
-void printStringAndInt(char* str, int val, int row)
-{
+void printStringAndInt(char *str, int val, int row) {
   display_goto_xy(0, row);
   display_string(str);
   display_int(val, 0);
   display_update();
 }
 
-TASK(FeedingTask)
-{
-  while (1)
-  {
-    rotateMotor(100, 40, 90, 1, NXT_PORT_A);
+TASK(FeedingTask) {
+  while (1) {
+    rotateMotorByDegrees(100, 40, 90, 1, NXT_PORT_A);
   }
   TerminateTask();
 }
