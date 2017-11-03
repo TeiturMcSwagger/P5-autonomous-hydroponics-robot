d140656506c482854d67c1181a1a1539d637653f
Lau Kondrup <lau.kondrup@gmail.com>
2017-11-03 14:26:39 +0100

    util.printInt

--- src/util.c
+++ src/util.c
@@ -3,23 +3,28 @@
 #include "kernel_id.h"
 #include <stdlib.h>
 
-int displayCount = 0;
+int displayRow = 0;
 
 void printString(char *str) {
-  display_goto_xy(0, displayCount++);
-  display_string(str);
-  display_update();
+    display_goto_xy(0, displayRow++);
+    display_string(str);
+    display_update();
+}
+
+void printInt(int value) {
+    display_goto_xy(0, displayRow++);
+    display_int(value, 0);
+    display_update();
 }
 
 void printStringAndInt(char *str, int val) {
-  display_goto_xy(0, displayCount++);
-  display_string(str);
-  display_int(val, 0);
-  display_update();
+    display_goto_xy(0, displayRow++);
+    display_string(str);
+    display_int(val, 0);
+    display_update();
 }
 
-void clearScreen()
-{
+void clearScreen() {
     display_clear(0);
     displayCount = 0;
 }
--- src/util.h
+++ src/util.h
@@ -1,5 +1,6 @@
 void printString(char *str);
 void printStringAndInt(char *str, int val);
 void clearScreen();
+void printInt(int value);
 
 extern int displayCount;
