d2ededbde430aaab1ad54387155fbd7c5dd207bb
Peter Madsen <pmadse13@student.aau.dk>
2017-11-03 09:01:35 +0100

    added motorControllerfunc to src

---
+++ src/nxtMotorController.h
@@ -0,0 +1,7 @@
+#include "types.h"
+void rotateMotorToAngle(int speedPercent, int brakeLength, U32 targetAngle,
+                        U32 motorPort, int allowedDeviation);
+
+void rotateMotorByDegrees(int speedPercent, int brakeLength,
+                          U32 degreesToRotate, bool turnDirection,
+                          U32 motorPort, int allowedDeviation);
