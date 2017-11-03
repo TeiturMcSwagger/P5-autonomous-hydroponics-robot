13fd6beb118e553f43809db9a9285d955a9cfc8a
Lau Kondrup <lau.kondrup@gmail.com>
2017-11-03 09:13:16 +0100

    Light sensor test working

--- nxtcolorsensortest/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload NXT_COLOR.rxe...
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=NXT_COLOR.rxe
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=NXT_COLOR.rxe
-echo NeXTTool is terminated.
