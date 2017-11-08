034ad021d07cf25120c15a30d145b8d1cbf491f1
Matias Jensen <sejemus@yahoo.dk>
2017-11-08 09:29:17 +0100

    Fixed turn.c

--- src/turn.c
+++ src/turn.c
@@ -5,27 +5,11 @@
 #include "path.h"
 #include "sym.h"
 
-int translationFactor = MAX_STEERING_ANGLE / OPTIMAL_LIGHT_VALUE;
-
 void turnMe() {
-    int lightValue = 100;
-    // nxt_motor_set_count(0, 180);
-
     while (1) {
-        if (lightValue <= OPTIMAL_LIGHT_VALUE) {
-            /*MAX_STEERING_ANGLE-(lightValue*translationFactor) is the amount of
-             * degress to be turned*/
-
-            rotateMotorToAngle(
-                100, 0, (MAX_STEERING_ANGLE - (lightValue * translationFactor)),
-                NXT_PORT_A, 0);
-
-        } else if (lightValue >= OPTIMAL_LIGHT_VALUE) {
-            /*The bitch should turn all sails due east, cuz shit's about to go
-             * down.*/
-            rotateMotorToAngle(
-                100, 0, (MAX_STEERING_ANGLE - (lightValue * translationFactor)),
-                NXT_PORT_A, 0);
-        }
+        //Rotate motor to angle based on lightValue
+        rotateMotorToAngle(100, 0,
+            MAX_STEERING_ANGLE - (lightValue * TRANSLATIONFACTOR),
+            NXT_PORT_A, 0);
     }
 }
