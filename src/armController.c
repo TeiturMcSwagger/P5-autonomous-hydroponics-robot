ee4e521eea0317bbb25e29e7e82db72dbdc12c8c
Peter Madsen <pmadse13@student.aau.dk>
2017-11-15 14:50:37 +0100

    adds readability, our types

--- src/armController.c
+++ src/armController.c
@@ -3,15 +3,17 @@
 #include "kernel_id.h"
 #include "nxtMotorController.h"
 #include "sym.h"
+#include "types.h"
 
-int areWeFeeding;
+#include "types.h"
+bool areWeFeeding;
 
 void feed(nutrition n) { n.feedProc(n.amount); }
 void feedPills(void *amount) {
-    areWeFeeding = 1;
+    areWeFeeding = TRUE;
     int countOfPills = *((U8 *)amount);
     while (countOfPills-- > 0) {
         rotateMotorByDegrees(100, 90, TRUE, ARM_MOTOR_PORT);
     }
-    areWeFeeding = 0;
+    areWeFeeding = FALSE;
 }
