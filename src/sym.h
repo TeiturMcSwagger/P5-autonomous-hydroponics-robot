f39208995e52fe31442aedd04f64123d084711dc
Teitur <teiturargeseloy@hotmail.com>
2017-11-15 12:40:04 +0100

    Vi kan dreje armen nu og stope

--- src/sym.h
+++ src/sym.h
@@ -1,7 +1,10 @@
 #ifndef SYM_H
 #define SYM_H
 
-#define ARM_MOTOR_PORT 0
+#define ARM_MOTOR_PORT NXT_PORT_S3
+#define PATH_SENSOR_PORT NXT_PORT_S1
+#define PLANT_SENSOR_PORT NXT_PORT_S2
+
 
 #define RED_AMOUNT 1
 #define GREEN_AMOUNT 2
@@ -13,7 +16,6 @@ extern int translationfactor;
 #define MAX_SPEED_BRAKE_LENGTH 40
 #define MAX_STEERING_ANGLE 40
 #define DEFAULT_TURN_POSITION 180
-#define PATH_SENSOR_PORT NXT_PORT_S1
-#define PLANT_SENSOR_PORT NXT_PORT_S2
+
 
 #endif
