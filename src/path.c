cda22042a173edd02fb734c1d7ba3b7068200736
Lau Kondrup <lau.kondrup@gmail.com>
2017-11-21 15:37:45 +0100

    tweaking PID

--- src/path.c
+++ src/path.c
@@ -3,12 +3,13 @@
 #include "kernel_id.h"
 #include "sym.h"
 #include "util.h"
-#define PROPORTIONAL 0.5
-#define INTEGRAL 0.3
-#define DERIVATIVE 0.2
+#define PROPORTIONAL 0.2
+#define INTEGRAL 0.1
+#define DERIVATIVE 0.4
 
 int getLight();
 int getAverageLightValue(int numLoops);
+void turn(double pid);
 
 double integral = 0;
 int previousError = 0;
@@ -18,17 +19,44 @@ void followLine() {
     int error = getLight() - OPTIMAL_LIGHT;
     double proportional = error * PROPORTIONAL;
     integral = integral + error;
-    double integ = integral * INTEGRAL;
+    // double integ = integral * INTEGRAL;
     double derivative = (error - previousError) * DERIVATIVE;
-    double pid = proportional + integ + derivative;
+    double pid = proportional + derivative;
     previousError = error;
     turn(pid);
 }
 
+void turn(double pid) {
+    printStringAndInt("PID: ", pid);
+
+    const int baseSpeed = 20;
+    const int maxSpeed = 50;
+
+    int leftSpeed = baseSpeed + pid;
+    int rightSpeed = baseSpeed - pid;
+
+    if(leftSpeed > maxSpeed) {
+        leftSpeed = maxSpeed;
+    }
+    if(rightSpeed > maxSpeed) {
+        rightSpeed = maxSpeed;
+    }
+
+    if(leftSpeed < -maxSpeed) {
+        leftSpeed = -maxSpeed;
+    }
+    if(rightSpeed < -maxSpeed) {
+        rightSpeed = -maxSpeed;
+    }
+
+    nxt_motor_set_speed(LEFT_MOTOR, leftSpeed, 1);
+    nxt_motor_set_speed(RIGHT_MOTOR, rightSpeed, 1);
+}
+
 void stopDriving() {
     int speedPercent = 0;
-    nxt_motor_set_speed(RIGHT_MOTOR, speedPercent, 1);
     nxt_motor_set_speed(LEFT_MOTOR, speedPercent, 1);
+    nxt_motor_set_speed(RIGHT_MOTOR, speedPercent, 1);
 }
 
 int getLight() {
